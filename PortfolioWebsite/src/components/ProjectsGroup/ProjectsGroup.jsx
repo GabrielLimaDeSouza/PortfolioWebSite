29496ca57b4a62610398cdb2e96aeab3fee91031
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-08-07 12:49:56 -0300

    [UPDATE] Refatoração projects

--- PortfolioWebsite/src/components/ProjectsGroup/ProjectsGroup.jsx
+++ PortfolioWebsite/src/components/ProjectsGroup/ProjectsGroup.jsx
@@ -16,12 +16,16 @@ const ProjectsGroup = () => {
   return (
     <div className={styles.projectGroup}>
         <div className={styles.projectGroupContainer}>
+          <div className={styles.firstLine}>
             <ProjectCard className={styles.projectCard} img={protonImg} title="ProtOn" description="Sistema de recomendação de protocolos odontológicos" languages={['Javascript', 'React', 'CSS', 'MongoDB', 'NodeJS']} link="https://github.com/GabrielLimaDeSouza/ProtOn" />
             <ProjectCard className={styles.projectCard} img={atelierImg} title="Atelier Picinin" description="Sistema de divulgação e gerenciamento do Atelier Picinin" languages={['Javascript', 'React', 'CSS', 'MongoDB', 'NodeJS']} link="https://github.com/GabrielLimaDeSouza/Atelier-Picinin" />
             <ProjectCard className={styles.projectCard} img={monedasImg} title="Mone.das" description="Sistema de moedas estudantil" languages={['Javascript', 'React', 'CSS', 'MongoDB', 'NodeJS']} link="https://github.com/GabrielLimaDeSouza/Mone.das" />
+          </div>
+          <div className={styles.secondLine}>
             <ProjectCard className={styles.projectCard} img={medsImg} title="MEDS" description="Projeto frontend de um sistema hospitalar" languages={['HTML', 'CSS', 'Javascript']} link="https://github.com/GabrielLimaDeSouza/MEDS" />
             <ProjectCard className={styles.projectCard} img={arenaImg} title="Arena" description="Sistema de organização de partidas esportivas" languages={['HTML', 'CSS', 'Javascript','NodeJS']}  link="https://github.com/ICEI-PUC-Minas-PPLES-TI/plf-es-2022-1-ti2-0924100-arena" />
             <ProjectCard className={styles.projectCard} img={weatherappImg} title="WeatherApp" description="Site de metereologia integrado com a API do OpenWeatherApp" languages={['Javascript', 'React', 'CSS']} link="https://github.com/GabrielLimaDeSouza/React/tree/main/WeatherApp" />
+          </div>   
         </div>
     </div>
   )

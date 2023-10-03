f8cedf50bd5025647764b3726ec893cb59577a92
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-10-03 11:23:08 -0300

    fix: form validation

--- PortfolioWebsite/src/components/Form/Form.jsx
+++ PortfolioWebsite/src/components/Form/Form.jsx
@@ -4,11 +4,12 @@ import React from 'react'
 import styles from './Form.module.css'
 
 const Form = () => {
+
   return (
     <form action="https://formspree.io/f/mqkvelyb" className={styles.divForm} method='POST'>
-        <textarea type="text" name="name" id="inputName" className={styles.inputName} placeholder="Nome" />
-        <textarea type="email" name="email" id="inputEmail" className={styles.inputEmail} placeholder='Email'/>
-        <textarea type="text" name="message" id="inputMessage" className={styles.inputMessage} placeholder='Mensagem'/>
+        <textarea type="text" name="name" id="inputName" className={styles.inputName} placeholder="Nome" required />
+        <textarea type="email" name="email" id="inputEmail" className={styles.inputEmail} placeholder='Email' required />
+        <textarea type="text" name="message" id="inputMessage" className={styles.inputMessage} placeholder='Mensagem' required />
         <input type="hidden" name="_language" value="pt-BR" />
         <button type="submit" className={styles.btnSubmit}>Enviar Mensagem</button>
     </form>

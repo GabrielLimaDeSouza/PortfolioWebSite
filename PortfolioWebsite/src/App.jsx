eb61d62486c83a2aa47776d6bcc031f31a2d69b5
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-10-10 18:45:04 -0300

    feat: definição das rotas

--- PortfolioWebsite/src/App.jsx
+++ PortfolioWebsite/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// Styles
 import './App.css'
+
+// Pages
 import Home from './pages/Home/Home'
+import AboutMe from './pages/AboutMe/AboutMe'
+import Projects from './pages/Projects & Designs/Projects/Projects'
+import Designs from './pages/Projects & Designs/Designs/Designs'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/aboutMe' element={<AboutMe />} />
+          <Route exact path='/projects' element={<Projects />} />
+          <Route exact path='/designs' element={<Designs />} />
+        </Routes>
+      </Router>
     </>
   )
 }
